ee428404adca998fdaec0f286d2a453c585d9c32
Mathieu Lajoie <mlajoie03@gmail.com>
2019-05-11 22:40:48 -0400

    setup theming + localization

--- eternity/src/app/app.component.ts
+++ eternity/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding, OnDestroy, OnInit} from '@angular/core';
+import {Language, LocaleService} from 'angular-l10n';
+
+const THEMES: string[] = [
+    'material-light',
+    'material-dark'
+];
 
 @Component({
     selector: 'et-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+    @Language()
+    lang: string;
+
+    @HostBinding('class')
+    activeTheme: string = 'material-light';
+    
+    availableThemes: string[] = THEMES;
+
+    constructor(public locale: LocaleService) { }
+
+    ngOnInit(): void {}
+
+    ngOnDestroy(): void {}
+
+    selectLanguage(language: string): void {
+        this.locale.setCurrentLanguage(language);
+    }
 
 }
--- eternity/src/app/app.module.ts
+++ eternity/src/app/app.module.ts
@@ -2,16 +2,53 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatGridListModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
+import {L10nConfig, L10nLoader, LogLevel, ProviderType, StorageStrategy, TranslationModule} from "angular-l10n";
+import {HttpClientModule} from "@angular/common/http";
+
+const l10nConfig: L10nConfig = {
+    logger: {
+        level: LogLevel.Warn
+    },
+    locale: {
+        languages: [
+            { code: 'en', dir: 'ltr' },
+            { code: 'it', dir: 'ltr' }
+        ],
+        language: 'en',
+        storage: StorageStrategy.Cookie
+    },
+    translation: {
+        providers: [
+            { type: ProviderType.Static, prefix: './assets/locale-' }
+        ],
+        caching: true,
+        composedKeySeparator: '.',
+        missingValue: 'No key'
+    }
+};
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        TranslationModule.forRoot(l10nConfig),
+        MatToolbarModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatGridListModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
+    constructor(public l10nLoader: L10nLoader) {
+        this.l10nLoader.load();
+    }
 }
