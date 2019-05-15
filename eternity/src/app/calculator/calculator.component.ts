785b34ca69fc4bd12126114bdfbbb0dfcc84e749
Mathieu Lajoie <mlajoie03@gmail.com>
2019-05-14 21:58:42 -0400

    added calculator table

--- eternity/src/app/calculator/calculator.component.ts
+++ eternity/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Language } from 'angular-l10n';
 
 @Component({
@@ -12,8 +12,25 @@ export class CalculatorComponent implements OnInit, OnDestroy {
 
     public equation: string = '';
 
+    public calculatorRows = [
+        {},
+    ];
+
+    // NAH. MAKE AN ARRAY 9 to 1 for numbers.
+
     ngOnInit() {}
 
     ngOnDestroy() {}
+
+    pressButton(button: string | number): void {
+        this.equation += button;
+    }
+
+    @HostListener('keydown', ['$event'])
+    keydownListener(event: KeyboardEvent): void {
+        if (event.key === 'Backspace' && this.equation && this.equation.length > 0) {
+            this.equation = this.equation.slice(0, -1);
+        }
+    }
 
 }
