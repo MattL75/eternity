84a5b4e915b399e1d83ce40adc79d8c0c4839869
Jason Brennan <brenjaso@gmail.com>
2019-06-03 15:07:01 -0400

    changed call of absolute value

--- eternity/src/app/functions/square-root.ts
+++ eternity/src/app/functions/square-root.ts
@@ -1,7 +1,9 @@
+import { abs } from './abs';
+
 export function squareRoot(x: number, x0: number): number {
     let x1 = (x0 + (x / x0)) / 2.0;
 
-    if (Math.abs(x1 - x0) < Number.EPSILON) {
+    if (abs(x1 - x0) < Number.EPSILON) {
         return x1;
     } else {
         return squareRoot(x, x1);
