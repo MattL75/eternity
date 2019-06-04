098bfb737449f135a744ed382c76a3b0e55b4108
manel-gm <manel.gm@gmail.com>
2019-06-04 09:07:15 -0400

    review changes

--- eternity/src/app/functions/floor.ts
+++ eternity/src/app/functions/floor.ts
@@ -1,44 +1,41 @@
 export function floor(val: number): number {
-	let i: number = 1;
-	let prev: number = 1;
-	let	result: number = 0;
-	let prevResult: number = 0;
-	
-	if (val > 0) {
-		while (val > result){
-			if (val >= prev){
-				result = prev;
-				prev = prev + i;
-				i = i * 2;
-			}
-			else {
-				if (prevResult == result){
-					return result;
-				}
-				prevResult = result;
-				i = 1;
-				prev = result;
-			}
-		}
-	}
-	else if (val < 0) {
-		prev = -1;
-		while (val < result){
-			if (val <= prev){
-				result = prev;
-				prev = prev - i;
-				i = i * 2;
-			}
-			else {
-				if (prevResult == result){
-					return result;
-				}
-				prevResult = result;
-				i = 1;
-				prev = result;
-			}
-		}
-	}
-	
-	return result;
+    let i: number = 1;
+    let prev: number = 1;
+    let result: number = 0;
+    let prevResult: number = 0;
+
+    if (val > 0) {
+        while (val > result) {
+            if (val >= prev) {
+                result = prev;
+                prev = prev + i;
+                i = i * 2;
+            } else {
+                if (prevResult == result) {
+                    return result;
+                }
+                prevResult = result;
+                i = 1;
+                prev = result;
+            }
+        }
+    } else if (val < 0) {
+        prev = -1;
+        while (val < result) {
+            if (val <= prev) {
+                result = prev;
+                prev = prev - i;
+                i = i * 2;
+            } else {
+                if (prevResult == result) {
+                    return result;
+                }
+                prevResult = result;
+                i = 1;
+                prev = result;
+            }
+        }
+    }
+
+    return result;
 }
